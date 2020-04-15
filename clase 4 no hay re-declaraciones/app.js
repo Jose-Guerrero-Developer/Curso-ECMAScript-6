706f7456674428b9b0bf2de7dbd7a1f46d9e70ed
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-04-15 10:09:46 -0500

    Clase 4 no existe la re-declaración de variables con let

---
+++ clase 4 no hay re-declaraciones/app.js
@@ -0,0 +1,21 @@
+var name = 'Susuna Alexandra'
+var name = 'Alexandra Kamila'
+
+console.log(name)
+
+/** detiene la ejecucuón de programa */
+// let name = 'Paola Alejandra'
+// console.log(name)
+
+/** existe en el scope global */
+let message = 'Hola, buenos días'
+if (true) {
+    /** existe en scope de el if */
+    let message = 'Estamos estudiando'
+}
+console.log(message)
+
+/**
+Alexandra Kamila
+Hola, buenos días
+ */
