ded41e208688a0ef8d698256f5c5907fe0794b20
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-04-15 17:55:00 -0500

    Clase 12 usando valores raw string

---
+++ clase 12 usando valores raw string/app.js
@@ -0,0 +1,11 @@
+var message  = 'Hola, \nbuenos días'
+var message2 = String.raw`Hola \nbuenos días`
+
+console.log(message)
+console.log(message2)
+
+/**
+Hola, 
+buenos días
+Hola \nbuenos días
+ */
