6708f530019eac7b884170c45f0c918fb6fa8c7f
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-04-18 10:46:29 -0500

    Clase 29 metodos concisos

---
+++ clase 29 metodos concisos/app.js
@@ -0,0 +1,28 @@
+/** ES5 */
+var person = {
+    age: 18,
+    name: 'Susana Alexandra López Medina',
+    city: 'Bogotá',
+    info: function () {
+        return this.name + ' (' + this.age + ')' + this.city
+    }
+}
+
+console.log(person.info())
+
+/** ES6 */
+var otherPerson = {
+    age: 18,
+    name: 'Susana Alexandra López Medina',
+    city: 'Bogotá',
+    info() {
+        return this.name + ' (' + this.age + ')' + this.city
+    }
+}
+
+console.log(otherPerson.info())
+
+/**
+Susana Alexandra López Medina (18)Bogotá
+Susana Alexandra López Medina (18)Bogotá
+ */
