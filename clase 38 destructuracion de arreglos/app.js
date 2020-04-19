681117226eb00affe900415b62987cea56712a9a
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-04-19 16:26:04 -0500

    Clase 38 destructuración de arreglos

---
+++ clase 38 destructuracion de arreglos/app.js
@@ -0,0 +1,20 @@
+let fruit = ['banano', 'pera', 'uva']
+
+let [banano, pera] = fruit
+
+console.log(banano, pera)
+
+let [ , , uva] = fruit
+
+console.log(uva)
+
+let otherFruit = 'manzana'
+console.log('other fruit:', otherFruit)
+
+let a = 1
+let b = 3;
+
+[a, b] = [b, a]
+
+console.log('a:', a)
+console.log('b:', b)
