7b546e7d863fc1f4889001576ea48f22152318aa
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-04-21 13:07:27 -0500

    Clase 66 sobreescribiendo funciones de el padré

---
+++ clase 66 sobreescribiendo funciones del padre/app.js
@@ -0,0 +1,29 @@
+class Retangulo {
+    constructor (high, long) {
+        this.high = high
+        this.long = long
+    }
+    getArea () {
+        return `Retangulo: ${ (this.high * this.long) }`
+    }
+}
+
+class Cuadrado extends Retangulo {
+    constructor (high) {
+        super(high, high)
+    }
+    getArea () {
+        // return super.getArea()
+        return `Cuadrado: ${ (this.high * this.long) }`
+    }
+}
+
+let retangulo = new Retangulo(3, 2)
+let cuadrado  = new Cuadrado(5)
+
+console.log(cuadrado.getArea())
+
+/**
+retangulo.getArea(): 6
+cuadrado.getArea(): 25
+ */
